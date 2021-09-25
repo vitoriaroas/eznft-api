a206ef74b03dded733ee6eb3be2e4eb0ea71363d
Vitoria <vitoriaroas@gmail.com>
2021-09-25 17:58:49 -0400

    added cors

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,15 @@
 const functions = require("firebase-functions")
 const express = require("express")
-
+const cors = require("cors")
 const app = express()
 
-const { addArtwork, getArtwork, newUser} = require("./src/index")
+const { addArtwork, getArtwork, newUser } = require("./src/index")
 
-app.get("/artwork/:artId", getArtwork);
-app.post('/artwork', addArtwork)
-app.post("/users", newUser);
+app.use(cors())
+
+app.get("/artwork/:artId", getArtwork)
+app.post("/artwork", addArtwork)
+app.post("/users", newUser)
 // app.post("/buyers/:userId", updateUser);
 
 exports.app = functions.https.onRequest(app)
